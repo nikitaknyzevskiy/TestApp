17f4dd62346e76137fab584df0717a5676f3bcdc
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:34:01 +0200

    Implemented user dao

--- app/src/main/java/com/nikita/testapp/db/AppDatabase.kt
+++ app/src/main/java/com/nikita/testapp/db/AppDatabase.kt
@@ -3,11 +3,15 @@ package com.nikita.testapp.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.nikita.testapp.db.convector.ImageConvector
 import com.nikita.testapp.db.convector.NameConvector
+import com.nikita.testapp.db.dao.UserDao
 import com.nikita.testapp.db.model.UserModel.UserModel
 
 @Database(entities = [UserModel::class], version = 1)
-@TypeConverters(NameConvector::class)
+@TypeConverters(NameConvector::class, ImageConvector::class)
 abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
 
 }
