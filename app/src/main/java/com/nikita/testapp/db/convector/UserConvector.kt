f94fb5b293f529fef5c8b3c8767309410a7bda77
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:38:30 +0200

    Created user convector

---
+++ app/src/main/java/com/nikita/testapp/db/convector/UserConvector.kt
@@ -0,0 +1,28 @@
+package com.nikita.testapp.db.convector
+
+import com.nikita.testapp.db.dto.UserDto
+import com.nikita.testapp.db.model.UserModel.UserModel
+
+object UserConvector {
+
+    fun convert(userDto: UserDto): UserModel {
+        return UserModel(
+            userDto.id.value,
+            userDto.gender,
+            userDto.name,
+            userDto.email,
+            userDto.phone,
+            userDto.cell,
+            userDto.picture
+        )
+    }
+
+    fun convert(userDtos: List<UserDto>): List<UserModel> {
+        val data = ArrayList<UserModel>()
+        for (userDto in userDtos) {
+            data.add(convert(userDto))
+        }
+        return data
+    }
+
+}
