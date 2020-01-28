b55b7cc6ebe39801cd6ab677930c5a573a488bb0
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:22:20 +0200

    Added id

--- app/src/main/java/com/nikita/testapp/db/model/UserModel/UserModel.kt
+++ app/src/main/java/com/nikita/testapp/db/model/UserModel/UserModel.kt
@@ -1,11 +1,18 @@
 package com.nikita.testapp.db.model.UserModel
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.nikita.testapp.db.model.ext.LargeModel
 import com.nikita.testapp.db.model.ext.NameModel
 
 @Entity
 data class UserModel (
+
+    @PrimaryKey
+    @NonNull
+    val id: String,
+
     val gender: String,
 
     val name: NameModel,
