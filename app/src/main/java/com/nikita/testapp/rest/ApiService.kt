ea1f5eb2b412577ada63de804d6d6fffcd721653
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:11:30 +0200

    Get users from api

--- app/src/main/java/com/nikita/testapp/rest/ApiService.kt
+++ app/src/main/java/com/nikita/testapp/rest/ApiService.kt
@@ -1,5 +1,11 @@
 package com.nikita.testapp.rest
 
+import com.nikita.testapp.db.model.UserModel.UserModel
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET("api")
+    suspend fun userList(): List<UserModel>
 
 }
