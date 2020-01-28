41525d63c63d5558ce178bb3d7731c3b9f45bac2
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:39:43 +0200

    Created base response

--- app/src/main/java/com/nikita/testapp/rest/ApiService.kt
+++ app/src/main/java/com/nikita/testapp/rest/ApiService.kt
@@ -3,10 +3,11 @@ package com.nikita.testapp.rest
 import com.nikita.testapp.db.dto.UserDto
 import com.nikita.testapp.db.model.UserModel.UserModel
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("api")
-    suspend fun userList(): List<UserDto>
+    @GET("api/")
+    suspend fun userList(@Query("results") results: Int = 20): BaseResponse<List<UserDto>>
 
 }
---
+++ app/src/main/java/com/nikita/testapp/rest/BaseResponse.kt
@@ -0,0 +1,3 @@
+package com.nikita.testapp.rest
+
+data class BaseResponse <RESULT> (val results: RESULT)
