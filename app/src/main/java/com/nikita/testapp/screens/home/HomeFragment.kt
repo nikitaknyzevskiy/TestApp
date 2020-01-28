3fbb8b8d3f7d48924306ef2cc9e698a173ea4580
nikita <nekbakht.zabirov.triare@gmail.com>
2020-01-28 15:57:00 +0200

    Created recycle list

--- app/src/main/java/com/nikita/testapp/screens/home/HomeFragment.kt
+++ app/src/main/java/com/nikita/testapp/screens/home/HomeFragment.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.nikita.testapp.R
 import com.nikita.testapp.screens.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -24,6 +27,15 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        home_list.layoutManager = LinearLayoutManager(this.context!!).apply {
+            orientation = RecyclerView.VERTICAL
+        }
+
+
     }
 
 
